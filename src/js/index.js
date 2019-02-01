0d40d56469732ff0749e33df267b18d3676535ac
Vitaliy <plachyndav@gmail.com>
2019-02-01 16:39:29 +0200

    complete menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
 "use strict";
+
+var menuButton = document.querySelector(".js-button");
+var menu = document.querySelector(".js-menu");
+menuButton.addEventListener("click", function (e) {
+    console.log('hi');
+    e.preventDefault();
+    menu.classList.toggle("main-nav__list--closed");
+});
